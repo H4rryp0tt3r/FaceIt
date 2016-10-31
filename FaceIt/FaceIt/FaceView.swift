377cc3d15b611d06d1be1a8ac03c30fbfcdd5cc1
H4rryp0tt3r <nagesh.podilapu@gmail.com>
2016-10-31 18:11:35 +0530

    Add eyes to FaceView by calculating eyeCenter with respective to faceRadius & faceCenter

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -1,12 +1,59 @@
 import UIKit
 
 class FaceView: UIView {
+
+    let scale: CGFloat = 0.90
+
+    var faceRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+
+    var faceCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+
+    private struct Ratios {
+        static let FaceRadiusToEyeOffSet: CGFloat = 3
+        static let FaceRadiusToEyeRadius: CGFloat = 10
+        static let FaceRadiusToMouthWidth: CGFloat = 1
+        static let FaceRadiusToMouthHeight: CGFloat = 3
+        static let FaceRadiusToMouthRadius: CGFloat = 3
+    }
+
+    private enum Eye {
+        case Left
+        case Right
+    }
+
+    private func getEyeCenter(eye: Eye) -> CGPoint {
+        let eyeOffset = faceRadius / Ratios.FaceRadiusToEyeOffSet
+        var eyeCenter = faceCenter
+        eyeCenter.y -= eyeOffset
+        switch eye {
+        case .Left:
+            eyeCenter.x -= eyeOffset
+        default:
+            eyeCenter.x += eyeOffset
+        }
+        return eyeCenter
+    }
+
+    private func pathForEye(eye: Eye) -> UIBezierPath {
+        let eyeRadius = faceRadius / Ratios.FaceRadiusToEyeRadius
+        let eyeCenter = getEyeCenter(eye: eye)
+        return pathForCircleCenteredAtPoint(midPoint: eyeCenter, withRadius: eyeRadius)
+    }
+
+    private func pathForCircleCenteredAtPoint(midPoint: CGPoint, withRadius radius: CGFloat) -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: midPoint, radius: radius, startAngle: 0.0, endAngle: CGFloat(2 * M_PI), clockwise: true)
+        path.lineWidth = 3.0
+        return path
+    }
+
     override func draw(_ rect: CGRect) {
-        let faceRadius = min(bounds.size.width, bounds.size.height) / 2
-        let faceCenter = CGPoint(x: bounds.midX, y: bounds.midY)
-        let face = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0.0, endAngle: CGFloat(2 * M_PI), clockwise: true)
-        face.lineWidth = 3.0
         UIColor.blue.set()
-        face.stroke()
+        pathForCircleCenteredAtPoint(midPoint: faceCenter, withRadius: faceRadius).stroke()
+        pathForEye(eye: .Left).stroke()
+        pathForEye(eye: .Right).stroke()
     }
 }
