6fd276eb9075c39590aebe675898dd8bdcfab484
H4rryp0tt3r <nagesh.podilapu@gmail.com>
2016-10-31 18:42:47 +0530

    Add mouth to face by drawing a beizerpath connecting 4 points

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -44,6 +44,29 @@ class FaceView: UIView {
         return pathForCircleCenteredAtPoint(midPoint: eyeCenter, withRadius: eyeRadius)
     }
 
+    private func pathForMount() -> UIBezierPath {
+        let mouthWidth = faceRadius / Ratios.FaceRadiusToMouthWidth
+        let mouthHeight = faceRadius / Ratios.FaceRadiusToMouthHeight
+        let mouthOffSet = faceRadius / Ratios.FaceRadiusToMouthRadius
+
+        let mouthRect = CGRect(x: faceCenter.x - mouthWidth / 2, y: faceCenter.y + mouthOffSet, width: mouthWidth, height: mouthHeight)
+
+        let mouthCurvature: Double = 1.0
+
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.minY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.minY)
+        let cp1 = CGPoint(x: mouthRect.minX + mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+        let cp2 = CGPoint(x: mouthRect.maxX - mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = 3.0
+
+        return path
+    }
+
     private func pathForCircleCenteredAtPoint(midPoint: CGPoint, withRadius radius: CGFloat) -> UIBezierPath {
         let path = UIBezierPath(arcCenter: midPoint, radius: radius, startAngle: 0.0, endAngle: CGFloat(2 * M_PI), clockwise: true)
         path.lineWidth = 3.0
@@ -55,5 +78,6 @@ class FaceView: UIView {
         pathForCircleCenteredAtPoint(midPoint: faceCenter, withRadius: faceRadius).stroke()
         pathForEye(eye: .Left).stroke()
         pathForEye(eye: .Right).stroke()
+        pathForMount().stroke()
     }
 }
